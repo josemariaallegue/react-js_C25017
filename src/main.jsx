27bd95e2cdc57ac1991e55309fc119793c989368
José M <inudzuka@DESKTOP-NLE6721.localdomain>
2025-06-24 23:29:55 -0300

    Eliminacion del CartProvider de esta ubicación. Se paso a App.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { CardProvider } from "./context/CartContext";
 import "./css/normalize.css";
 import "./css/index.css";
 
 createRoot(document.getElementById("root")).render(
-  <CardProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </CardProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
