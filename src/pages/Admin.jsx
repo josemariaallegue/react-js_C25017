8ef9d3ea41b80cb90741c60b643fae2cd9b294b6
José M <inudzuka@DESKTOP-NLE6721.localdomain>
2025-07-19 18:26:23 -0300

    Implementacion del crud contra mockapi.

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,17 +2,19 @@ import ProductForm from "../components/ProductForm";
 import ProductTable from "../components/ProductTable";
 import { useState, useRef } from "react";
 import { toast } from "react-toastify";
+import { useProducts } from "../context/ProductsContext";
 
 export default function Admin() {
   const [initialData, setInitialData] = useState(null);
   const [mode, setMode] = useState("create");
   const miDivRef = useRef(null);
+  const { addProduct, deleteProduct, updateProduct } = useProducts();
 
-  function handleSubmit() {
+  function handleSubmit(product) {
     if (!initialData) {
-      toast.success("Creado");
+      addProduct(product);
     } else {
-      toast.warn("Modificado");
+      updateProduct(product.id, product);
     }
     setInitialData(null);
     setMode("create");
@@ -20,9 +22,16 @@ export default function Admin() {
 
   function handleEdit(product) {
     setInitialData({
+      id: product.id,
       title: product.title,
       price: product.price,
       description: product.description,
+      category: product.category,
+      image: product.image,
+      rating: {
+        rate: product.rating.rate,
+        count: product.rating.count,
+      },
     });
     setMode("edit");
     miDivRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -38,7 +47,7 @@ export default function Admin() {
         onSubmit={handleSubmit}
         mode={mode}
       />
-      <ProductTable onEdit={handleEdit} />
+      <ProductTable onEdit={handleEdit} onDelete={deleteProduct} />
     </main>
   );
 }
